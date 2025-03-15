ae7cb65ff7f737f29aab2ccd95e5ba83f7d06f0e
Gildemberg Vieira Lopes <gildembergvieiralopes@gmail.com>
2025-03-15 18:02:08 -0300

    Criação da Screen Menu Inicial

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import React from 'react';
 import Boasvindas from './screens/boasVindas';
 import Login from './screens/login';
 import CadastrarConta from './screens/cadastrarConta';
+import MenuInicial from './screens/menuInicial';
 
 function App() {
 
   return (
-    <CadastrarConta/>
+    <MenuInicial/>
   );
 }
 
