d6fc3e6e64a2493ba1695c2d67e4c9bd924344fa
Gildemberg Vieira Lopes <gildembergvieiralopes@gmail.com>
2025-03-15 21:08:18 -0300

    Criação da Screen Caixa de Entrade e do Component Card Notificação

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Login from './screens/login';
 import CadastrarConta from './screens/cadastrarConta';
 import MenuInicial from './screens/menuInicial';
 import Ouvidoria from './screens/ouvidoria';
+import CaixaDeEntrada from './screens/caixaDeEntrada';
 
 function App() {
 
   return (
-    <Ouvidoria/>
+    <CaixaDeEntrada/>
   );
 }
 
