25d2510a809e91bbded898100af4f063cb5a02a5
Gildemberg Vieira Lopes <gildembergvieiralopes@gmail.com>
2025-04-20 13:05:14 -0300

    Criação da Screen Localização

---
+++ src/screens/localizacao/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import css from './style';
+import Corpo from  './../../components/corpoDois';
+
+export default props => {
+    return (
+        <Corpo>
+            <View style={css.container}>
+                <Text style={css.titulo}>Localização</Text>
+
+                <View style={css.mapa}></View>
+
+                <Text style={css.subtitulo}>---- OU SE PREFIRIR, DIGITE O ENDEREÇO ----</Text>
+
+                <Text style={css.label}>Rua/Avenida</Text>
+                <TextInput style={css.input} placeholder="Ex.: Rua Freitas Cavalvente, 454"/>
+
+                <Text style={css.label}>Bairro</Text>
+                <TextInput style={css.input} placeholder="Ex.: Centro"/>
+
+                <Text style={css.label}>Referência</Text>
+                <TextInput style={css.input} placeholder="Ex.: Próximo ao Posto Shell"/>
+
+                
+                <TouchableOpacity style={css.btnProximo}><Text style={css.txtBtnProximo}>PRÓXIMO</Text></TouchableOpacity>
+                
+            </View>
+        </Corpo>
+    )
+}
---
+++ src/screens/localizacao/style.js
@@ -0,0 +1,56 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+    container: {
+        flex:1
+    },
+    titulo:{
+        fontSize: 40,
+        marginBottom: 40,
+        alignSelf: 'center',
+        color: '#5d5d5d',
+        fontWeight: '300'
+    },
+    mapa:{
+        backgroundColor: '#5d5d5d',
+        height:150,
+        borderRadius: 10,
+    },
+    subtitulo:{
+        textAlign: 'center',
+        marginTop:20,
+        marginBottom:20,
+        fontSize:16,
+        width: '100%',
+    },
+    label: {
+        marginBottom:5
+    },
+    input: {
+        borderWidth: 1,
+        borderRadius:8,
+        borderColor: '#AAA',
+        marginBottom: 20,
+        backgroundColor: '#fff',
+        paddingLeft: 10
+    },
+    btnProximo:{
+        borderWidth: 1,
+        borderRadius:8,
+        backgroundColor: '#fff',
+        borderColor: '#5d5d5d',
+        padding:8,
+        shadowColor: "#000",
+        elevation: 3,
+        position: 'absolute',
+        bottom: 20,
+        width: '100%'
+        
+    },
+    txtBtnProximo:{
+        textAlign: 'center',
+        fontWeight: 'bold'
+    }
+})
+
+export default style;
