9beb248bf2a982abfdc916c1eae84fb526b95d4e
Gildemberg Vieira Lopes <gildembergvieiralopes@gmail.com>
2025-04-21 17:07:46 -0300

    Criação da Screen Mapa

---
+++ src/screens/mapa/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from 'react-native';
+import css from './style';
+
+export default function Resumo({ navigation }) {
+    return(
+        <View style={css.container}>
+
+        </View>
+    )
+}
