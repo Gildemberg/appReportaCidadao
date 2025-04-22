ff618783a1121d3600a1050a2531e83db80b2012
Gildemberg Vieira Lopes <gildembergvieiralopes@gmail.com>
2025-04-22 00:09:58 -0300

    Atualizando o StatusBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './navigation/routes'
 
 function App() {
   return (
     <NavigationContainer>
+      <StatusBar backgroundColor="rgba(0, 0, 0, 0.3)" 
+                 barStyle="light-content" 
+                 translucent={true} 
+      />
       <Routes/>
     </NavigationContainer>
   );
